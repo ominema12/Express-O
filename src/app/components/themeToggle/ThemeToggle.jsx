328cb8536b92ba6a1b39a3e4dde7e152aa9eda06
ominema12 <ominema12@gmail.com>
2024-03-02 22:12:42 +0530

    add : test commit

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "./themeToggle.module.css"
+import Image from "next/image";
 
 const ThemeToggle = () => {
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from "./homepage.module.css";
 import Featured from "./components/featured/Featured";
 import CategoryList from "./components/categoryList/CategoryList";
 import CardList from "./components/cardList/CardList";
-
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
 
@@ -12,8 +12,8 @@ export default function Home() {
     <Featured/>
     <CategoryList/>
     <div className={styles.content}>
- <CardList/>
- <Menu/>
+      <CardList/>
+      <Menu/>
     </div>
   </div>
    
